d55f58f487bf6d5a0343f75610f73057710ceb6f
Fábio - IPCOM <fabio@ipcom.com.br>
2020-01-22 11:41:07 -0300

    Initial commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e3f785f13758d8',
+    pass: 'e419860f8f313e',
+  },
+  default: {
+    from: 'Equipe IPCOM <noreply@ipcom.com.br>',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://7ae02f57b93249658e900e2bda559b54@sentry.io/1893553',
+};
